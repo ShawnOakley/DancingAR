d9130ea276d58df37741896962d8d76602a94d5d
Shawn Oakley <shawnoakley@gmail.com>
2018-10-31 03:31:11 -0400

    Init commit with model and anim

---
+++ Assets/Scripts/zombieController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombieController : MonoBehaviour {
+
+	public GameObject Zombie;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = Zombie.getComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void changeAnim(){
+		Debug.Log("PRESSED");
+	}
+}
